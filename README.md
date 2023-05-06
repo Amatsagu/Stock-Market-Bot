e49ae4df37d1dad5c25cfdfdb35fe920255805f7
amatsagu <krzysztof.rapior@protonmail.com>
2023-05-06 12:41:54 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Stock Market Discord Bot
+This application is a Discord bot that automatically retrieves stock market data from a https://marketstack.com (premium) API and creates graphs based on the stock data. It was created specifically for Vironicer from the Market Mornings team (https://marketmornings.com/).
+
+## Features
+* Retrieves End of Day (EOD) and yearly stock market data from an online API,
+* Automatically creates a graphs using stock market data,
+* Sends updated stock information to previously designated Discord server text channels
--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"flag"
 	"index-bot/logger"
-	"os"
-
-	godotenv "github.com/joho/godotenv"
 )
 
 func main() {
@@ -13,16 +9,16 @@ func main() {
 	logger.Info.Println("Starting Index bot application...")
 }
 
-func loadEnv() {
-	var filename string
-	flag.StringVar(&filename, "env", ".env", "switches which environmental file should be used (default: .env)")
-	flag.Parse()
+// func loadEnv() {
+// 	var filename string
+// 	flag.StringVar(&filename, "env", ".env", "switches which environmental file should be used (default: .env)")
+// 	flag.Parse()
 
-	if err := godotenv.Load(filename, ".env"); err != nil {
-		logger.Error.Panic(err) // Unrecoverable!
-	}
+// 	if err := godotenv.Load(filename, ".env"); err != nil {
+// 		logger.Error.Panic(err) // Unrecoverable!
+// 	}
 
-	if _, available := os.LookupEnv("APP_TOKEN"); !available {
-		logger.Error.Panic("environmental file is missing credentials (invalid .env file)")
-	}
-}
+// 	if _, available := os.LookupEnv("APP_TOKEN"); !available {
+// 		logger.Error.Panic("environmental file is missing credentials (invalid .env file)")
+// 	}
+// }
